47ee0e3a35afac5d4cf60dec5a0cc84beee5ae3e
Ilya Anikeyenka <20prorok02@gmail.com>
2023-03-05 02:22:34 +0300

    Solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (y > 0) {
+            if (x > 0) {
+                System.out.println("first");
+            } else if (x < 0) {
+                System.out.println("second");
+            }
+        } else if (y < 0) {
+            if (x > 0) {
+                System.out.println("fourth");
+            } else if (x < 0) {
+                System.out.println("third");
+            }
+        } else {
+            System.out.println("zero");
+        }
     }
 }
